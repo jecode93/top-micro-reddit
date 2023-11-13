7d6ba30cd331ee99bb9e57cf722c76d225598f88
jecode93 <jeanemmanuelcadet@gmail.com>
2023-11-12 19:30:46 -0500

    Complete comment model, add validations and associations for Comment to Post and User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 	
 	has_many :posts
+	has_many :comments
 	
 	validates :username, presence: true, length: { in: 4..12 }, uniqueness: true
 	validates :email, presence: true, uniqueness: true
