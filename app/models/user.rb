13c430d66c7a5bd8b285c5206c0fbb0a0209c688
jecode93 <jeanemmanuelcadet@gmail.com>
2023-11-12 19:03:17 -0500

    Complete post modle, add validations and associations between Post and User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+	
+	has_many :posts
+	
 	validates :username, presence: true, length: { in: 4..12 }, uniqueness: true
 	validates :email, presence: true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_12_231506) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_12_233745) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "link"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -21,4 +31,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_12_231506) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
